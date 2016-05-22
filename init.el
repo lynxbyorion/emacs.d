3424a4326fa9e577fb5fa6b846fd160c65ae67da
Gennady Sazonov <lynxbyorion@gmail.com>
2016-05-22 14:40:33 +0300

    Remove auto-generated comments.

--- init.el
+++ init.el
@@ -1,7 +1,5 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;; Bref: Emacs init dot file.
+;; Author: lynxbyorion.
 
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
